e556c9bdb5968a183d84365cdf3951060275805d
Fajar Karim <fajar.karim@btpn.com>
2019-08-01 16:23:07 +0700

    initialize

---
+++ src/main/java/com/btpn/fajar/trybasemasterdata/TrybasemasterdataApplication.java
@@ -0,0 +1,13 @@
+package com.btpn.fajar.trybasemasterdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrybasemasterdataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrybasemasterdataApplication.class, args);
+    }
+
+}
