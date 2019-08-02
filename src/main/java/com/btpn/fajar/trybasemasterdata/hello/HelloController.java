f343975c7f9f3b4a6ad6c95783773ff6d8385297
Fajar Karim <fajar.karim@btpn.com>
2019-08-02 09:54:03 +0700

    add pdf xml file

--- src/main/java/com/btpn/fajar/trybasemasterdata/hello/HelloController.java
+++ src/main/java/com/btpn/fajar/trybasemasterdata/hello/HelloController.java
@@ -1,13 +1,30 @@
 package com.btpn.fajar.trybasemasterdata.hello;
 
+import net.sf.jasperreports.engine.*;
+import net.sf.jasperreports.engine.util.JRSaver;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Base64;
 
 @RestController
 public class HelloController {
 
+    @Autowired
+    private ResourceLoader resourceLoader;
+
     @GetMapping(path="/hello")
-    public String hello () {
-        return "hello world";
+    public String hello () throws JRException, IOException {
+        InputStream pdfStream = resourceLoader.getResource("classpath:jasper/hello.jrxml").getInputStream();
+        JasperReport pdfReport = JasperCompileManager.compileReport(pdfStream);
+        JRSaver.saveObject(pdfReport, "hello.jasper");
+        JasperPrint pdfPrint = JasperFillManager.fillReport(pdfReport, null, new JREmptyDataSource());
+        byte[] pdfBytes = JasperExportManager.exportReportToPdf(pdfPrint);
+        String blob = Base64.getEncoder().encodeToString(pdfBytes);
+        return blob;
     }
 }
