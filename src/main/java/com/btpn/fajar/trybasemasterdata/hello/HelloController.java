8a85ec6b21b2721cf78b60932a8325657f35a506
Fajar Karim <fajar.karim@btpn.com>
2019-08-13 11:17:27 +0700

    create dto file, create jasper to receive paramters

--- src/main/java/com/btpn/fajar/trybasemasterdata/hello/HelloController.java
+++ src/main/java/com/btpn/fajar/trybasemasterdata/hello/HelloController.java
@@ -4,13 +4,16 @@ import net.sf.jasperreports.engine.*;
 import net.sf.jasperreports.engine.util.JRSaver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ResourceLoader;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Base64;
+import java.util.HashMap;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class HelloController {
 
@@ -18,13 +21,23 @@ public class HelloController {
     private ResourceLoader resourceLoader;
 
     @GetMapping(path="/hello")
-    public String hello () throws JRException, IOException {
+    public HelloDTO hello () throws JRException, IOException {
+        // Read jrxml file
         InputStream pdfStream = resourceLoader.getResource("classpath:jasper/hello.jrxml").getInputStream();
+
+        // create jasper report
         JasperReport pdfReport = JasperCompileManager.compileReport(pdfStream);
-        JRSaver.saveObject(pdfReport, "hello.jasper");
-        JasperPrint pdfPrint = JasperFillManager.fillReport(pdfReport, null, new JREmptyDataSource());
+
+        // create parameters for jasper file, for example key parameter FAJAR_TITLE with "junkies" as value
+        HashMap<String, Object> mainParameters = new HashMap<>();
+        mainParameters.put("FAJAR_TITLE", "junkies");
+        JasperPrint pdfPrint = JasperFillManager.fillReport(pdfReport, mainParameters, new JREmptyDataSource());
+
+        // convert jasper file to pdf
         byte[] pdfBytes = JasperExportManager.exportReportToPdf(pdfPrint);
+
+        // convert pdf to base64
         String blob = Base64.getEncoder().encodeToString(pdfBytes);
-        return blob;
+        return new HelloDTO(blob);
     }
 }
